56c1a169c9d3c87527d5f72e4ea059054fbbbbd0
Heti <prosto.lyubo@gmail.com>
2021-07-24 14:10:19 +0200

    Sound Hub for player

---
+++ Assets/Scripts/SoundHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SoundHub : MonoBehaviour
+{
+    public UnityEvent OnPlayerJumped;
+    public UnityEvent OnPlayerHit;
+    public UnityEvent OnPlayerDefeated;
+    public UnityEvent OnPlayerVictorious;
+}
