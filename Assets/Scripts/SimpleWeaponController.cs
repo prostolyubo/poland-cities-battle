8773ebf7ac5e6df0a9f81e16e3e99e28addbc2d0
mkonr4d <mkonr4d@gmail.com>
2021-07-24 16:11:24 +0200

    atak syrenki
    dzwiek "siuuuuuuu"

--- Assets/Scripts/SimpleWeaponController.cs
+++ Assets/Scripts/SimpleWeaponController.cs
@@ -3,11 +3,13 @@ using System.Collections;
 using UnityEngine;
 public class SimpleWeaponController : WeaponController
 {
+    public AK.Wwise.Event Play_sound;
     void Update()
     {
         if (!isLocked && Input.GetKeyDown(ActionKeyCode))
         {
             Use();
+            Play_sound.Post(gameObject);
         }
     }
 }
